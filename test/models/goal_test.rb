28f9e89259a4f259c5a1b9634c71584707521c40
Leo <leocsdev@gmail.com>
2021-05-29 11:06:37 +0800

    Created model test for goals.

--- test/models/goal_test.rb
+++ test/models/goal_test.rb
@@ -4,4 +4,11 @@ class GoalTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'Should not save Goals without title' do
+    goal = Goal.new
+    goal.description = 'Model TDD Test'
+
+    assert_not goal.save, "Saved Goals without title"
+  end
 end
