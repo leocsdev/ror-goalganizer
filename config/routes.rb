7a54231890a8199864e2b119f7aa209d11dc6ead
Leo <leocsdev@gmail.com>
2021-05-29 10:40:37 +0800

    Added home controller to test tailwind.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # get 'home/index'
+  # get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
+
+  get 'about', to: 'home#about'
 end
