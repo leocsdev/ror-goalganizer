68dee55892409a725e177f6631f2572cf13d7151
Leo <leocsdev@gmail.com>
2021-06-01 19:34:24 +0800

    Fixed home controller test.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,12 +2,14 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
+    # get home_index_url
+    get goals_path
     assert_response :success
   end
 
   test "should get about" do
-    get home_about_url
+    # get home_about_url
+    get about_path
     assert_response :success
   end
 end
