ffbf321c7495d3b270f6167f525b402700b4f2bd
Leo <leocsdev@gmail.com>
2021-06-01 14:32:02 +0800

    Added controller action for goal's show, edit, update, delete.

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -17,6 +17,32 @@ class GoalsController < ApplicationController
     end
   end
 
+  def show
+    @goal = Goal.find(params[:id])
+  end
+
+  def edit
+    @goal = Goal.find(params[:id])
+  end
+
+  def update
+    @goal = Goal.find(params[:id])
+
+    if @goal.update(goal_params)
+      # redirect_to goals_path
+      redirect_to goal_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @goal = Goal.find(params[:id])
+    @goal.destroy
+
+    redirect_to goals_path
+  end
+
   private
   def goal_params
     params.require(:goal).permit(:title, :description)
