2be430b748ac112a53dedec5a55d2a2528a55087
Leo <leocsdev@gmail.com>
2021-06-05 13:04:06 +0800

    Added index method to show current user's tasks.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
+  # before_action :authenticate_user!
+
   def index
-    @goals = Goal.all
+    # @goals = Goal.all
+    if current_user
+      @tasks = current_user.tasks
+    end
   end
 
   def about
