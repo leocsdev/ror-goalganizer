eaaeaeb41a09f9e0954d112df9930b4d2b06620a
Leo <leocsdev@gmail.com>
2021-06-05 17:28:26 +0800

    Display tasks due today in Homepage if user is logged in.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,13 @@ class HomeController < ApplicationController
   # before_action :authenticate_user!
 
   def index
-    # @goals = Goal.all
+    # If user is logged in
     if current_user
-      @tasks = current_user.tasks
+      # get all tasks due today
+      @tasks = current_user.tasks.where(
+        deadline: Date.current, 
+        status: 'Started'
+      )
     end
   end
 
