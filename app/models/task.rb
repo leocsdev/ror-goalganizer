d6a4f39b45ed844aa26b62996dbf994250625221
Leo <leocsdev@gmail.com>
2021-06-07 22:07:41 +0800

    Added validation for task name, deadline, status. User will not be able to save task if due date is in the past.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,15 @@
 class Task < ApplicationRecord
-  validates :name,
+  validates :name, :deadline, :status,
     presence: true
 
-    # Associate with goal
-    belongs_to :goal
+  validate :date_cannot_be_in_the_past
+  
+  def date_cannot_be_in_the_past
+    if deadline.present? && deadline < Date.today
+      errors.add("Due date", " can't be in the past")
+    end
+  end
+
+  # Associate with goal
+  belongs_to :goal
 end
