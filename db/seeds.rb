402b5fa2293fa159b2c4501a362d01876cbcfd49
leocsdev <leocsdev@gmail.com>
2023-06-16 09:41:51 +0800

    b001, added nodejs specific version, pry gem, and seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.where(email: 'asdf@asdf.com').first_or_initialize
+
+user.update!(
+  password: 'asdfasdf',
+  password_confirmation: 'asdfasdf',
+)
